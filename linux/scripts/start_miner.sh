7a1aa829715a12319101b42d4bcaca3f318b34b6
Orelious Washington <codes@owash.me>
2025-11-16 20:04:18 -0800

    Update start_miner.sh

--- linux/scripts/start_miner.sh
+++ linux/scripts/start_miner.sh
@@ -3,5 +3,5 @@ SESSION="miner"
 
 tmux has-session -t $SESSION 2>/dev/null
 if [ $? != 0 ]; then
-    tmux new-session -d -s $SESSION "cd /home/ubuntu/xmrig && ./xmrig"
+    tmux new-session -d -s $SESSION "cd /home/ubuntu/xmrig/build && ./xmrig"
 fi
